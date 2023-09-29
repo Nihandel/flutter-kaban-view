fb72f339d45bbdeaf36f2971ee390e47c32e71ea
francesco finotto <finottofrancesco1@gmail.com>
2023-09-29 18:46:14 +0200

    commented kabanColumnHeader

--- kanban_view/lib/kanban_column.dart
+++ kanban_view/lib/kanban_column.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-class KabanColumnHeader extends StatelessWidget {
-  const KabanColumnHeader({super.key, this.title, this.trailing, this.bottom});
-  final Widget? title;
-  final Widget? trailing;
-  final PreferredSizeWidget? bottom;
-  @override
-  Widget build(BuildContext context) {
-    return SliverAppBar(
-      expandedHeight: 70,
-      title: title,
-      floating: true,
-      pinned: true,
-      actions: trailing != null ? List.from([trailing]) : [],
-      bottom: bottom,
-    );
-  }
-}
+// class KabanColumnHeader extends StatelessWidget {
+//   const KabanColumnHeader({super.key, this.title, this.trailing, this.bottom});
+//   final Widget? title;
+//   final Widget? trailing;
+//   final PreferredSizeWidget? bottom;
+//   @override
+//   Widget build(BuildContext context) {
+//     return SliverAppBar(
+//       expandedHeight: 70,
+//       title: title,
+//       floating: true,
+//       pinned: true,
+//       actions: trailing != null ? List.from([trailing]) : [],
+//       bottom: bottom,
+//     );
+//   }
+// }
 
 class KabanColumn extends StatelessWidget {
   const KabanColumn(
