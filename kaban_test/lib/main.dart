5f1230a83943883dd9476081d8ae3dab3588ec27
francesco finotto <finottofrancesco1@gmail.com>
2023-09-29 16:30:15 +0200

    flex power 2

--- kaban_test/lib/main.dart
+++ kaban_test/lib/main.dart
@@ -13,42 +13,27 @@ class TestKaban extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var random = Random();
-    return LayoutBuilder(
-      builder: (context,constraints) {
-        return Stack(
-          children: [
-            Positioned(
-              top: 50,
-              left: 50,
-              width: constraints.maxWidth *.8,
-              height: constraints.maxHeight *.8,
-              child: KanbanView(
-                children: List.generate(
-                  10,
-                  (index) => KabanColumn(
-                    flex: index.toDouble(),
-                    minWidth: 300,
-                    maxWidth: 500,
-                    color: Colors.red,
-                    title: const Text("Column 1"),
-                    trailing: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.menu),
-                    ),
-                    children: List.generate(
-                      9,
-                      (index) => const SizedBox(
-                        height: 200,
-                        child: Text("Test"),
-                      ),
-                    ),
-                  ),
-                ).toList(),
-              ),
+    return KanbanView(
+      children: List.generate(
+        7,
+        (index) => KabanColumn(
+          flex: index.toDouble()*index.toDouble(),
+          minWidth: 100,
+          color: Colors.red,
+          title: const Text("Column 1"),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu),
+          ),
+          children: List.generate(
+            9,
+            (index) => const SizedBox(
+              height: 200,
+              child: Text("Test"),
             ),
-          ],
-        );
-      }
+          ),
+        ),
+      ).toList(),
     );
   }
 }
